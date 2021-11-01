6f0c1633a7b0cea705e9a2e3af629aa36af04ee2
salman602 <salmanrahman602@gmail.com>
2021-11-01 23:32:34 +0600

    added email filter and token authentication

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('dotenv').config()
+require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
 const app = express();
@@ -13,15 +13,15 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v2tgv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run (){
-    try{
+async function run() {
+    try {
         await client.connect();
         const database = client.db("online_shop");
         const productCollection = database.collection("products");
         const orderCollection = database.collection("orders");
 
         // GET products API
-        app.get('/products', async(req, res)=>{
+        app.get('/products', async (req, res) => {
             const cursor = productCollection.find({});
             // console.log(req.query);
             const page = req.query.page;
@@ -32,11 +32,11 @@ async function run (){
             let products;
             const count = await cursor.count();
             if (page) {
-                products = await cursor.skip(page*size).limit(size).toArray();
+                products = await cursor.skip(page * size).limit(size).toArray();
             } else {
                 products = await cursor.toArray();
             }
-            
+
             res.send({
                 count,
                 products
@@ -44,32 +44,49 @@ async function run (){
         });
 
         // use POST to get products by keys
-        app.post('/products/byKeys', async (req, res) =>{
+        app.post('/products/byKeys', async (req, res) => {
             console.log(req.body);
             const keys = req.body;
-            const query = {key: {$in: keys}};
+            const query = { key: { $in: keys } };
             const products = await productCollection.find(query).toArray();
             res.json(products);
         });
 
         // Order post
-        app.post('/orders', async(req, res) => {
+
+        app.get('/orders', async (req, res) => {
+            let query = {};
+            const email = req.query.email;
+
+            if (email) {
+                query = {email: email}
+            }
+            const cursor = orderCollection.find(query);
+
+            console.log(query)
+
+            const result = await cursor.toArray();
+            res.json(result);
+        })
+
+        app.post('/orders', async (req, res) => {
             const order = req.body;
+            order.createdAt = new Date();
             // console.log(order);
             const result = await orderCollection.insertOne(order);
             res.json(result);
         })
     }
-    finally{
+    finally {
         // await client.close();
     }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Ema john is running on the server side..')
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
